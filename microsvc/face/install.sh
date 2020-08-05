e1b4d6ab534fbdfcd980edb2a7d12732738abdae
Emmanuel Rayendra <emmanuelr.2018@smu.edu.sg>
2020-08-05 11:19:40 +0800

    Updated install.sh to include running files from current dir

--- microsvc/face/install.sh
+++ microsvc/face/install.sh
@@ -1,1 +1,3 @@
-pip3 install --no-cache-dir -r requirements.txt
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+pip3 install --no-cache-dir -r $parent_path/requirements.txt
